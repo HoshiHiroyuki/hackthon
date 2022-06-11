14edde0b625c4eb7bd21bfad966a1c8bf86a96bd
Hiroyuki Hoshi <hoshih@qunie.com>
2022-06-11 08:28:29 +0000

    v2
    Signed-off-by: Hiroyuki Hoshi <hoshih@qunie.com>

---
+++ srv/server.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const cds = require("@sap/cds");
+const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+const cors = require("cors");
+
+cds.on("bootstrap", app => app.use(proxy()));
+cds.on("bootstrap", app => app.use(cors())); 
+
+module.exports = cds.server;
